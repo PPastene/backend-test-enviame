1768f26d47b800c1ca32e926efbd8fe61091cfc9
Pedro Pablo Pastene <pe.pastene@gmail.com>
2021-04-26 13:37:59 -0400

    Se agreganuevo campo y validacion queda como variable

--- ejercicio_1_2_4/src/app/Http/Controllers/EmpresaController.php
+++ ejercicio_1_2_4/src/app/Http/Controllers/EmpresaController.php
@@ -8,11 +8,13 @@ use App\Models\Empresa;
 
 class EmpresaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $validacion = [
+        'nombre' => 'required|string',
+        'direccion' => 'required|string',
+        'codigo_postal' => 'required',
+        'fono' => 'required|string'
+    ];
+
     public function index()
     {
         return response()->json(Empresa::all(), 200);
@@ -28,19 +30,9 @@ class EmpresaController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $validador = Validator::make($request->all(), [
-            'nombre' => 'required|string',
-            'direccion' => 'required|string',
-            'codigo_postal' => 'required'
-        ]);
+        $validador = Validator::make($request->all(), $this->validacion);
 
         if($validador->fails())
         {
@@ -54,6 +46,7 @@ class EmpresaController extends Controller
         $empresa->nombre = $request->nombre;
         $empresa->direccion = $request->direccion;
         $empresa->codigo_postal = $request->codigo_postal;
+        $empresa->fono = $request->fono;
 
         $empresa->save();
 
@@ -86,24 +79,9 @@ class EmpresaController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        if(!$id)
-        {
-            dd("test");
-        }
-        $validador = Validator::make($request->all(), [
-            'nombre' => 'required|string',
-            'direccion' => 'required|string',
-            'codigo_postal' => 'required'
-        ]);
+        $validador = Validator::make($request->all(), $this->validacion);
 
         if($validador->fails())
         {
@@ -124,6 +102,7 @@ class EmpresaController extends Controller
         $empresa->nombre = $request->nombre;
         $empresa->direccion = $request->direccion;
         $empresa->codigo_postal = $request->codigo_postal;
+        $empresa->fono = $request->fono;
 
         $empresa->save();
 
@@ -134,12 +113,6 @@ class EmpresaController extends Controller
         ], 201);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $empresa = Empresa::find($id);
